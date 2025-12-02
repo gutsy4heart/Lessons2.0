220f89c8cccd11fa7093404a9fd10efc59553cb0
Murad Nurmammadli <murad.nurmammadli@gmail.com>
2025-12-02 19:50:39 +0400

    add lessons

---
+++ C++/EgersLesson/EgersLesson/Intro.cpp
@@ -0,0 +1,14 @@
+﻿//#include<iostream>
+//
+//using namespace std;
+//
+//int main() 
+//{
+//	cout << "Hello World";		        // cout -> vivod na konsol
+//
+//	cout << endl;
+//	cout << "Bye bye";					// endl -> novaya stroka
+//	
+//
+//
+//}
---
+++ C++/EgersLesson/EgersLesson/Variables.cpp
@@ -0,0 +1,53 @@
+//#include<iostream>
+//using namespace std;
+//
+//
+//int main()
+//{
+// // datatype    variable_name			value
+//	int			num1					= 5;
+//
+//	// 0,1,3,4,5.... -> int
+//
+//	float num2 = 5.5f;
+//
+//	// 0.0,1.0,2.0,3.0,4.0,5.0... -> float
+//
+//	char name[20] = "Edgars";
+//
+//
+//	/*float a = 0;
+//	float b = 0;
+//
+//	cout << "Enter the a: ";
+//	cin >> a;
+//	cout << "Enter the b: ";
+//	cin >> b;
+//
+//	cout << "Add" << endl;
+//	cout << a + b << endl;
+//	cout << "Substract" << endl;
+//	cout << a - b << endl;
+//	cout << "Multiply" << endl;
+//	cout << a * b << endl;
+//	cout << "Divide" << endl;
+//	cout << a / b << endl;*/
+//
+//	// % -> ostatok ot deleniya
+//	 
+//	// == -> sravnivat na ravenstvo
+//	// != -> sravnivat na neravenstvo
+//	// = -> prisvaivat
+//	
+//	//bool a = true, b = false;
+//
+//	//int x = 10, y = 6;
+//
+//	//cout << (x % y == 0) << endl; // 1 - true
+//	//cout << (x % y != 0) << endl; // 0 - false
+//
+//
+//
+//
+//
+//}
